265a689171d89025d249701cb281c53b87ce9eaf
Rubpri <rubenprietoserrano@gmail.com>
2023-11-17 13:19:16 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  
+  const [advice, setAdvice] = useState({ slip: { advice: "..." } })
+  
+    const fetchAdvice = async () => {
+    
+    const url = "https://api.adviceslip.com/advice";
+    
+    const request = {
+      method: "GET"
+    }
+
+    const response = await fetch(url, request);
+
+    if (response.ok) {
+      const data = await response.json()
+      setAdvice(data)
+      
+    } else {
+      console.log("Error", response.status, response.statusText)
+    }
+  };
+
+  useEffect (() => {
+    fetchAdvice()
+  }, []);
+
+  const handleUpdate = () => {
+    fetchAdvice();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {advice.slip.advice && (
+        <>
+          <h4>Advice {advice.slip.id}</h4>
+          <h3>{advice.slip.advice}</h3>
+        </>
+      )} 
+
+      <div className="div-separator">
+        <div className="separator"></div>
+        <i className="fa-solid fa-pause"></i>
+        <div className="separator"></div>
+      </div>
+      
+      <i className="update fa-solid fa-rotate-right" onClick={handleUpdate}></i>
+      
     </div>
   );
 }
